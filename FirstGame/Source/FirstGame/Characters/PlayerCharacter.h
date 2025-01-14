ca1c4a26367b1f5e37035457fd6b1fa9d504558e
dem4ke <d3m4k3@gmail.com>
2025-01-14 18:21:32 +0300

    feat: add player character

---
+++ FirstGame/Source/FirstGame/Characters/PlayerCharacter.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FGBaseCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class FIRSTGAME_API APlayerCharacter : public AFGBaseCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	APlayerCharacter();
+
+	virtual void MoveForward(float Value) override;
+	virtual void MoveRight(float Value) override;
+	virtual void Turn(float Value) override;
+	virtual void LookUp(float Value) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Camera")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character | Camera")
+	USpringArmComponent* SpringArmComponent;
+};
